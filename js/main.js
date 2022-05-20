e1805b6597076b8d010509982cb8bfae79828f8c
PATRONCITOsky <hhernadez144@misena.edu.co>
2022-05-19 19:01:30 -0500

    Se agg varias funciones

--- js/main.js
+++ js/main.js
@@ -1,16 +1,25 @@
 const API = "https://api.github.com/users/";
 
-async function Buscar(){
-    const response = await fetch(API + 'PATRONCITOsky')
-    //ahora quiero traer la info en formato json
-    const data = await response.json()
-    console.log(data)
-}
+
+//se corto el metodo buscar
 
 const app = Vue.createApp({
     data() {
         return {
-          message: 'Hello SKY!'
+          message: 'Hello SKY!',
+          busqueda: null,
+          result: null
         }
+      },
+      
+      //La palabra function ya no es necesaria ya que se usa un metodo
+      methods: {
+        async  Buscar(){
+          const response = await fetch(API + this.busqueda)
+          //ahora quiero traer la info en formato json
+          const data = await response.json()
+          console.log(data)
+          this.result = true
+      }
       }
 })//se monta esta información en el html div app y ya se puede usar en el index
